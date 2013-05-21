7a5605fe241115db5691292fa209c3b1bd1c92dd
Jeff <jostendorf@gmail.com>
2013-05-21 08:21:32 -0500

    Added functionality for hover highlighting code blocks
    Requires using an additional argument into the pygments for the code block, this
    will wrap each line with an id of `name-[line #]`.  Then any links that target that
    anchor id or a range (`name-1:5` targets lines 1-5) will highlight the lines on
    hover.  The highlighting is done by adding/removing the `emphasize` class.  There
    are still some issues if the code block doesn't have the id-ed spans wrapping each
    line, but this could probably be fixed by looking for a `-` in the id of the span
    in the CSS (hacky).

--- s/js/cleanup.js
+++ s/js/cleanup.js
@@ -24,3 +24,51 @@ window.addEventListener("load", function () {
   });
 });
 
+window.addEventListener("load", function () {
+  var article = document.getElementById("body");
+  if (article.className.indexOf("post") === -1) { return; }
+
+  var links = article.getElementsByTagName("a");
+  for (var i = 0, l = links.length; i < l; i++) {
+    var link = links[i];
+    if (!link.getAttribute("href") || link.getAttribute("href")[0] !== "#") {
+      continue;
+    }
+
+    (function (l) {
+      var href = l.getAttribute("href").substr(1);
+
+      var id = href.substr(0, href.indexOf('-'));
+      var range = href.substr(href.indexOf('-') + 1);
+      if (range.indexOf(':') === -1) {
+        range = [parseInt(range, 10)];
+      } else {
+        var start = parseInt(range.substr(0, range.indexOf(':')), 10),
+          end = parseInt(range.substr(range.indexOf(':') + 1), 10);
+
+        range = [];
+
+        for (start; start <= end; start++) {
+          range.push(start);
+        }
+      }
+
+      var lines = [];
+
+      l.addEventListener("mouseover", function () {
+        for (var i = 0, l = range.length; i < l; i++) {
+          var line = document.getElementById(id + '-' + range[i]);
+          line.className = "emphasize";
+          lines.push(line);
+        }
+      });
+
+      l.addEventListener("mouseout", function () {
+        while (lines.length) {
+          var line = lines.pop();
+          line.className = "";
+        }
+      });
+    }(link));
+  }
+});
